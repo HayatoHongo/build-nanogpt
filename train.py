8fb2dfa683fc1b10647da204f7d49c289715923e
HayatoHongo <151999571+HayatoHongo@users.noreply.github.com>
2025-03-02 10:16:18 +0900

    val step from 250 to 4000

--- train.py
+++ train.py
@@ -394,7 +394,7 @@ for step in range(max_steps):
     last_step = (step == max_steps - 1)
 
     # once in a while evaluate our validation loss
-    if step % 250 == 0 or last_step:
+    if step % 4000 == 0 or last_step:
         model.eval()
         val_loader.reset()
         with torch.no_grad():
@@ -427,7 +427,7 @@ for step in range(max_steps):
                 torch.save(checkpoint, checkpoint_path)
 
     # once in a while evaluate hellaswag
-    if (step % 250 == 0 or last_step) and (not use_compile):
+    if (step % 4000 == 0 or last_step) and (not use_compile):
         num_correct_norm = 0
         num_total = 0
         for i, example in enumerate(iterate_examples("val")):
@@ -460,7 +460,7 @@ for step in range(max_steps):
                 f.write(f"{step} hella {acc_norm:.4f}\n")
 
     # once in a while generate from the model (except step 0, which is noise)
-    if ((step > 0 and step % 250 == 0) or last_step) and (not use_compile):
+    if ((step > 0 and step % 4000 == 0) or last_step) and (not use_compile):
         model.eval()
         num_return_sequences = 4
         max_length = 32
